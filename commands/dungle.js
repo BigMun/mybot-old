588f8beeba6bc33c106289aeea252a2a6b2e8b6e
BigMun <stephenhav921@gmail.com>
2020-01-12 02:38:57 +0000

    rename

--- commands/dungle.js
+++ commands/dungle.js
@@ -1,14 +1,15 @@
 const quickcord = require('quickcord');
 
 module.exports = {
-    name: "molest",
+    name: "dungled",
     call: (res, args) => {
         const member = res.mentions.members.first()
     
         res.delete(500);
     
         const embedi = new quickcord.Embed({
-            title: `${member.user.tag} is being molested`,
+            title: `${member.user.tag} is being dungled
+            `,
             description: 'Dutty bomba',
             color: '#bf2ac7'
         });
